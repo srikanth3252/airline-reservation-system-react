4b12d44d6feb1368bef19c4b115e12f6163cb064
srikanth3252 <srikanthreddybapatu04@gmail.com>
2026-08-02 18:59:44 +0530

    Added TiDB SSL configuration

--- server/db.js
+++ server/db.js
@@ -4,9 +4,14 @@ require("dotenv").config();
 
 const db = mysql2.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true
+    }
 });
 
 db.connect((err) => {
