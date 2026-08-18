10114517e82f50b87775bc4341a73a8e102ffcee
srikanth3252 <srikanthreddybapatu04@gmail.com>
2026-08-18 10:42:12 +0530

    Fix secure TiDB database connection

--- server/db.js
+++ server/db.js
@@ -4,14 +4,19 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 4000,
+
+    ssl: {
+        minVersion: "TLSv1.2"
+    }
 });
 
 db.connect((err) => {
 
     if (err) {
         console.log("Database connection failed");
-        console.log(err.message);
+        console.log(err);
         return;
     }
 
